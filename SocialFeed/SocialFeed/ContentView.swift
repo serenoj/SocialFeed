09adb6bbfff0e50c4a8233075c7e9e51eb65a81a
Juan Correa - iOS <juanetas@icloud.com>
2019-12-04 15:29:39 -0500

    Initial commit: Xcode project created

---
+++ SocialFeed/SocialFeed/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SocialFeed
+//
+//  Created by Juan Carlos Correa Arango on 4/12/19.
+//  Copyright © 2019 Sereno. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
